7cb273242a3aa85a7a26c303f97260ad1d530093
HubCJ <u20201b742@upc.edu.pe>
2023-02-11 19:48:14 -0500

    Add project files.

---
+++ Entidades/EConcepto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class EConcepto
+    {
+        public int Codigo { get; set; }
+        public string Concepto { get; set; }
+        public decimal Importe { get; set; }
+
+        public EConcepto() { }
+
+        public EConcepto(int codigo, string concepto, decimal importe)
+        {
+            Codigo = codigo;
+            Concepto = concepto;
+            Importe = importe;
+        }
+    }
+}
