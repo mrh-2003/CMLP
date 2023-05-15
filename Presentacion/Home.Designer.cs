23f1dc16f1114fc40ad8d4ddc15260897bc2a1ee
Jonas-bm <94211214+Jonas-bm@users.noreply.github.com>
2023-05-15 18:59:26 -0500

    cambiando el tamaño de letra

--- Presentacion/Home.Designer.cs
+++ Presentacion/Home.Designer.cs
@@ -570,13 +570,13 @@ namespace Presentacion
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(298, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(431, 17);
+            this.label1.Size = new System.Drawing.Size(325, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Creditos Jonas Barrientoz 977839158 / Huber Calizaya 957018079";
+            this.label1.Text = "Creditos: Jonas Barrientos 977839158 / Huber Calizaya 957018079";
             // 
             // menuKardex
             // 
